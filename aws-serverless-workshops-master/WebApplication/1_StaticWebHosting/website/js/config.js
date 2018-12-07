ea78f0098d0134b53f730904973a88b147233b1e
steave6 <todayonewintomorrowtwo@yahoo.co.jp>
2018-12-07 18:13:45 +0900

    initial commit for aws tutorials

---
+++ aws-serverless-workshops-master/WebApplication/1_StaticWebHosting/website/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-northeast-1_AOAIGJN9F', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2cfe87rd9chqhkb8uk4h74rcsm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://w6ocau9gm8.execute-api.ap-northeast-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
