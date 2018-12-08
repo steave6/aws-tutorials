df1a794e49877c1fd8811245f501191fcfb7cbdb
steave6 <todayonewintomorrowtwo@yahoo.co.jp>
2018-12-08 23:34:30 +0900

    #UPDATE make docker store data consistently

--- DynamoDB/Basic/docker-run.sh
+++ DynamoDB/Basic/docker-run.sh
@@ -1,3 +1,9 @@
 #! /usr/bin/env bash
 
-docker run -d -p 8000:8000 amazon/dynamodb-local
+docker run --rm -d -p 8000:8000 \
+  --name amazon-dynamodb \
+  --mount source=dynamodb-vol,target=/home/dynamodblocal \
+  amazon/dynamodb-local \
+  -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb
+
+
